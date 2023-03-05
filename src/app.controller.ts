25cbc09c30f8e03497504c2a2df54515bb68047e
Chopper <chopperxx@Cvetkovs-MacBook-Air.local>
2023-03-05 19:39:07 +0600

    Add validating paramets

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,14 @@
 import { Controller, Get, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 
+import { ParserDto } from './dto/parser.dto';
+
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  async getData(@Body() body): Promise<any> {
-    if (!body?.url) {
-      return 'Something wend wrong';
-    }
-
+  async getData(@Body() body: ParserDto): Promise<any> {
     return await this.appService.getHtmlByUrl(body.url);
   }
 }
